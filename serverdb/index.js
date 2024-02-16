8ef3cda854bf298b39b620950ede837d66907792
vishal <vishalms2342@gmail.com>
2024-02-16 11:49:09 +0530

    my changes

--- serverdb/index.js
+++ serverdb/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://fitness-tracki.vercel.app",
 		credentials: true,
 	})
 );
